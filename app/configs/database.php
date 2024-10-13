379f887ba5b74c031f294a7720e351ea70329aec
Hp2003 <panchalhirenm123@gmail.com>
2024-10-13 06:34:07 +0530

    added config file for db

---
+++ app/configs/database.php
@@ -0,0 +1,22 @@
+<?php 
+return [
+    /**
+     * holds name of db host
+     */
+    'host' => 'localhost',
+
+    /**
+     * holds name of db user
+     */
+    'name' => 'root',
+
+    /**
+     * hods password of db
+     */
+    'password' => 'panchal4555',
+
+    /**
+     * holds name of database
+     */
+    'db_name' => 'project1',
+];
