7fc55386f47de9342fcdb1c78172821d3f07c802
Hp2004 <panchalhirenm123@gmail.com>
2024-10-14 18:13:43 +0530

    fixed issue where having no records was showing table

--- app/views/course/index.php
+++ app/views/course/index.php
@@ -23,6 +23,9 @@ $courses = $course->get();
 
 <body>
   <?php $navbar?>
+  <?php if(count($courses ) <= 0){ ?>
+    <h1 class="text-center mt-5"> No records available :( </h1>
+  <?php }else{?>
   <div class="container mt-5">
     <table class="table table-striped">
       <thead>
@@ -62,6 +65,7 @@ $courses = $course->get();
       </tbody>
     </table>
   </div>
+  <?php } ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
--- app/views/student/index.php
+++ app/views/student/index.php
@@ -1,31 +1,40 @@
-<?php 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/hiren/mvc2/app/models/Student.php');
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/hiren/mvc2/app/models/Course.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/hiren/mvc2/app/models/Student.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/hiren/mvc2/app/models/Course.php');
 
-    $navbar = include_once('../nav.php');
+$navbar = include_once('../nav.php');
 
-    $studnet = new Student();
-    $students = $studnet->get();
-    $course = new Course();
-    $courses = $course->get_formatted_course();
+$studnet = new Student();
+$students = $studnet->get();
+$course = new Course();
+$courses = $course->get_formatted_course();
 
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
   <?php $navbar ?>
-    <div class="container mt-5">
-      <table class="table table-striped">
+  <div class="container mt-5">
+    <?php
+    if (count($students) <= 0) {
+    ?>
+      <h1 class="text-center mt-5"> No records available :( </h1>
+    <?php
+    }else{
+    ?>
+    <table class="table table-striped">
       <thead>
         <tr>
           <th scope="col">id</th>
@@ -43,31 +52,33 @@
         </tr>
       </thead>
       <tbody>
-        <?php foreach($students as $student){ ?>
-        <tr>
-          <th scope="row"><?php echo $student['id']?></th>
-          <td><?php echo $student['first_name']?></td>
-          <td><?php echo $student['last_name']?></td>
-          <td><?php echo $student['email']?></td>
-          <td><?php echo $student['gender']?></td>
-          <td><?php echo $courses[$student['course_id']] ?? 'N/A' ?></td>
-          <td><?php echo $student['course_id'] ? 'Active' : 'Inactive' ?></td>
-          <td><?php echo $student['phone_number'] ?></td>
-          <td><?php echo date('d-m-Y H : i', strtotime($student['created_at']))?></td>
-          <td><?php echo date('d-m-Y H : i', strtotime($student['updated_at']))?></td>
-          <td><a type="button" href="<?php echo "/hiren/mvc2/app/views/student/editStudent.php?id=" . $student['id']  ?>" class="btn btn-primary">Edit</a></td>
-          <td>
-            <form action="../../controllers/StudentController.php" method="POST">
+        <?php foreach ($students as $student) { ?>
+          <tr>
+            <th scope="row"><?php echo $student['id'] ?></th>
+            <td><?php echo $student['first_name'] ?></td>
+            <td><?php echo $student['last_name'] ?></td>
+            <td><?php echo $student['email'] ?></td>
+            <td><?php echo $student['gender'] ?></td>
+            <td><?php echo $courses[$student['course_id']] ?? 'N/A' ?></td>
+            <td><?php echo $student['course_id'] ? 'Active' : 'Inactive' ?></td>
+            <td><?php echo $student['phone_number'] ?></td>
+            <td><?php echo date('d-m-Y H : i', strtotime($student['created_at'])) ?></td>
+            <td><?php echo date('d-m-Y H : i', strtotime($student['updated_at'])) ?></td>
+            <td><a type="button" href="<?php echo "/hiren/mvc2/app/views/student/editStudent.php?id=" . $student['id']  ?>" class="btn btn-primary">Edit</a></td>
+            <td>
+              <form action="../../controllers/StudentController.php" method="POST">
                 <input type="hidden" name="id" value="<?php echo $student['id'] ?>">
                 <input type="hidden" name="operation" value="delete">
                 <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
-          </td>
-        </tr>
+              </form>
+            </td>
+          </tr>
         <?php } ?>
       </tbody>
     </table>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+  </div>
+  <?php } ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
 </html>
