bc99bf4f00be28ba2fef033e53f5518c3f270f15
Hp2004 <panchalhirenm123@gmail.com>
2024-10-16 15:08:50 +0530

    added filter has bugs wip

--- app/views/paginator.php
+++ app/views/paginator.php
@@ -7,27 +7,29 @@ $limit = 5;
 if(!empty($_GET['limit'])){
   $limit = $_GET['limit'];
 }
+$sort_by = !empty($_GET['sort_by']) ? $_GET['sort_by'] : "";
+$type = !empty($_GET['type']) ? $_GET['type'] : "";
 ?>
 <?php if ($pages['total_pages'] > 1) { ?>
     <div class="container d-flex justify-content-center h-auto user-select-none">
       <nav aria-label="Page navigation example">
         <ul class="pagination">
           <?php if ($pages['prev_page']) { ?>
-            <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=" . $pages['prev_page'] . "&limit=" . $limit ?>">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=" . $pages['prev_page'] . "&limit=" . $limit . "&sort_by=" . $sort_by . "&type=" . $type ?>">Previous</a></li>
             <?php if ($pages['from'] != 1) { ?>
-              <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=1" . "&limit=" . $limit ?>">1</a></li>
+              <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=1" . "&limit=" . $limit . "&sort_by=" . $sort_by . "&type=" . $type ?>">1</a></li>
               <li class="page-item"><a class="page-link disabled" href="#">...</a></li>
             <?php } ?>
           <?php } ?>
           <?php for ($page = $pages['from']; $page <= $pages['to']; $page++) { ?>
-            <li class="page-item"><a class="page-link <?php echo $current_page == $page ? 'active' : '' ?>" href="<?php echo $_SERVER['PHP_SELF'] . "?page=$page" . "&limit=" . $limit?>"><?php echo $page ?></a></li>
+            <li class="page-item"><a class="page-link <?php echo $current_page == $page ? 'active' : '' ?>" href="<?php echo $_SERVER['PHP_SELF'] . "?page=$page" . "&limit=" . $limit . "&sort_by=" . $sort_by . "&type=" . $type?>"><?php echo $page ?></a></li>
           <?php } ?>
           <?php if ($pages['next_page']) { ?>
             <?php if ($pages['to'] != $pages['total_pages']) { ?>
               <li class="page-item"><a class="page-link disabled" href="#">...</a></li>
-              <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=" . $pages['total_pages'] . "&limit=" . $limit  ?>"><?php echo $pages['total_pages'] ?></a></li>
+              <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=" . $pages['total_pages'] . "&limit=" . $limit . "&sort_by=" . $sort_by . "&type=" . $type  ?>"><?php echo $pages['total_pages'] ?></a></li>
             <?php } ?>
-            <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=" . $pages['next_page'] . "&limit=" . $limit ?>">Next</a></li>
+            <li class="page-item"><a class="page-link" href="<?php echo $_SERVER['PHP_SELF'] . "?page=" . $pages['next_page'] . "&limit=" . $limit . "&sort_by=" . $sort_by . "&type=" . $type ?>">Next</a></li>
           <?php } ?>
         </ul>
       </nav>
--- app/views/student/index.php
+++ app/views/student/index.php
@@ -16,11 +16,15 @@ $pattern = "/^(\d{3})(\d{3})(\d{4})$/";
 
 $student = new Student();
 $page = !empty($_GET['page']) ? $_GET['page'] : 1;
-$pagination_data = $student->paginate($page, $_GET['limit'] ?? 5);
+$pagination_data = $student->paginate($page, $_GET['limit'] ?? 5, $_GET['sort_by'] ?? "", $_GET['type'] ?? "");
 
 $pages = $pagination_data['pagination_numbers'] ?? 0;
 unset($pagination_data['pagination_numbers']);
 $students = $pagination_data;
+
+$sort_by = !empty($_GET['sort_by']) ? $_GET['sort_by'] : "";
+$type = !empty($_GET['type']) ? $_GET['type'] : "";
+$limit = $_GET['limit'] ?? 5;
 
 ?>
 <!doctype html>
@@ -43,31 +47,50 @@ $students = $pagination_data;
     <?php
     } else {
     ?>
-      <div class="container d-flex justify-content-end">
-        <form action="<?php echo $_SERVER['REQUEST_URI']  ?>" class="w-25 limit-form">
+      <div class="container d-flex justify-content-around">
+        <form action="<?php echo $_SERVER['REQUEST_URI'] ?>" class="w-25 limit-form">
+          <input type="hidden" name="sort_by" value=<?php echo $sort_by ?>>
+          <input type="hidden" name="type" value=<?php echo $type ?>>
           <select class="form-select limit" aria-label="Default select example" name="limit" onchange="submit()">
-            <option value="5" <?php echo ($_GET['limit'] ?? 5) == 5 ? 'selected' : '' ?>>5</option>
-            <option value="10" <?php echo ($_GET['limit'] ?? 5) == 10 ? 'selected' : '' ?>>10</option>
-            <option value="20" <?php echo ($_GET['limit'] ?? 5) == 20 ? 'selected' : '' ?>>20</option>
-            <option value="50" <?php echo ($_GET['limit'] ?? 5) == 50 ? 'selected' : '' ?>>50</option>
+            <option value="5" <?php echo $limit == 5 ? 'selected' : '' ?>>5</option>
+            <option value="10" <?php echo $limit == 10 ? 'selected' : '' ?>>10</option>
+            <option value="20" <?php echo $limit == 20 ? 'selected' : '' ?>>20</option>
+            <option value="50" <?php echo $limit == 50 ? 'selected' : '' ?>>50</option>
           </select>
+        </form>
+        <form action="<?php echo $_SERVER['REQUEST_URI']  ?>" class="w-25 limit-form d-flex">
+          <input type="hidden" name="limit" value="<?php echo $limit ?>">
+          <select class="form-select" aria-label="Default select example" name="sort_by">
+              <option value="id" >id</option>
+              <option value="first_name" <?php echo $sort_by === "first_name" ? 'selected' : '' ?> >first_name</option>
+              <option value="last_name" <?php echo $sort_by === "last_name" ? 'selected' : '' ?> >last_name</option>
+              <option value="email" <?php echo $sort_by === "email" ? 'selected' : '' ?> >email</option>
+              <option value="gender" <?php echo $sort_by === "gender" ? 'selected' : '' ?> >gender</option>
+              <option value="course" <?php echo $sort_by === "course" ? 'selected' : '' ?> >course</option>
+              <option value="status" <?php echo $sort_by === "status" ? 'selected' : '' ?> >status</option>
+              <option value="phone_number" <?php echo $sort_by === "phone_number" ? 'selected' : '' ?> >phone</option>
+              <option value="created_at" <?php echo $sort_by === "created_at" ? 'selected' : '' ?> >created_at</option>
+              <option value="updated_at" <?php echo $sort_by === "updated_at" ? 'selected' : '' ?> >updated_at</option>
+          </select>
+          <input type="submit" class="btn <?php $type === "ASC" ? "btn btn-primary" : "btn btn-warning" ?>" name="type" value="ASC">
+          <input type="submit" class="btn <?php $type === "DESC" ? "btn btn-primary" : "btn btn-warning" ?> " name="type" value="DESC">
         </form>
       </div>
       <table class="table table-striped">
         <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">first_name</th>
-            <th scope="col">last_name</th>
-            <th scope="col">email</th>
-            <th scope="col">gender</th>
-            <th scope="col">course</th>
-            <th scope="col">status</th>
-            <th scope="col">phone</th>
-            <th scope="col">created_at</th>
-            <th scope="col">updated_at</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
+          <tr class="user-select-none text-center">
+            <th scope="col " class="heading" onclick="filter_table(0)" >id</th>
+            <th scope="col " class="heading" onclick="filter_table(1)">first_name</th>
+            <th scope="col " class="heading" onclick="filter_table(2)">last_name</th>
+            <th scope="col " class="heading" onclick="filter_table(3)">email</th>
+            <th scope="col " class="heading" onclick="filter_table(4)">gender</th>
+            <th scope="col " class="heading" onclick="filter_table(5)">course</th>
+            <th scope="col " class="heading" onclick="filter_table(6)">status</th>
+            <th scope="col " class="heading" onclick="filter_table(7)">phone</th>
+            <th scope="col " class="heading" onclick="filter_table(8)">created_at</th>
+            <th scope="col " class="heading" onclick="filter_table(9)">updated_at</th>
+            <th scope="col " ></th>
+            <th scope="col " ></th>
           </tr>
         </thead>
         <tbody>
@@ -79,7 +102,7 @@ $students = $pagination_data;
               <td><?php echo $student['email'] ?></td>
               <td><?php echo $student['gender'] ?></td>
               <td><?php echo $courses[$student['course_id']] ?? 'N/A' ?></td>
-              <td><?php echo !empty($student['course_id']) ? 'Active' : 'Inactive' ?></td>
+              <td><?php echo $student['status'] ? 'Active' : 'Inactive' ?></td>
               <td><?php echo preg_replace($pattern, '$1-$2-$3', $student['phone_number']) ?></td>
               <td><?php echo date('d-m-Y h : i a', strtotime($student['created_at'])) ?></td>
               <td><?php echo date('d-m-Y h : i a', strtotime($student['updated_at'])) ?></td>
@@ -100,6 +123,19 @@ $students = $pagination_data;
   <?php require_once('../paginator.php') ?>
 <?php } ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<script>
+  function filter_table(index) {
+    let value = document.querySelectorAll('.heading')[index].textContent;
+    if(value === 'phone') value = 'phone_number';
+    console.log(value);
+    var currentURL=window.location.href.split('?')[0];
+    const urlParams = new URLSearchParams(window.location.search);
+    let type = urlParams.get('type') === "DESC" || urlParams.get('type') === "" ? 'ASC' : 'DESC';
+
+    window.location.href = (currentURL + "?sort_by=" + value + "&type=" + type);
+  }
+</script>
 </body>
 
 </html>
