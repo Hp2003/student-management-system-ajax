4b439aa910c3afee3fc663615d35bc04be9bc57e
Hp2004 <panchalhirenm123@gmail.com>
2024-10-24 16:00:38 +0530

    added display functionality in courses

--- public/js/courses.js
+++ public/js/courses.js
@@ -3,25 +3,52 @@
  *
  */
 const pageQueryStrings = {
-    currentPage: 1,
-    limit: 5,
-    sortby: 'id',
-    type: 'DESC',
+  currentPage: 1,
+  limit: 5,
+  sortby: 'id',
+  type: 'DESC',
 }
-getCourses();
+getCourses()
 /**
  * Function fetch students according to current page value
  *
  * @return void
  */
 function getCourses() {
-    setpageQueryStrings()
-    $.get(
-      `http://localhost/hiren/student_management_system/app/controllers/StudentController.php?page=${pageQueryStrings.currentPage}&limit=${pageQueryStrings.limit}&sort_by=${pageQueryStrings.sortby}&type=${pageQueryStrings.type}`,
-      function (data, status) {
-        if (status === 'success') {
-          //   console.log(data.pagination_data[0].first_name);
-        }
+  // setpageQueryStrings()
+  $.get(
+    `http://localhost/hiren/student_management_system/app/controllers/CourseController.php`,
+    function (data, status) {
+      if (status === 'success') {
+        displayCourse(data.pagination_data);
       }
-    )
+    }
+  )
+}
+
+/**
+ * Main function to display courses
+ *
+ * @param courses array
+ *
+ * @return void
+ */
+
+function displayCourse(courses) {
+    const tableBody = $('.table-body')
+    tableBody.empty()
+    courses.forEach((course) => {
+      let row = $('<tr></tr>')
+      row.append(`<th>${course.id}</th>`)
+      row.append(`<td>${course.name}</td>`)
+      row.append(`<td>${course.student_count}</td>`)
+      row.append(`<td>${course.created_at}</td>`)
+      row.append(`<td>${course.updated_at}</td>`)
+      row.append(`<td>
+              <button class="btn btn-primary">Edit</button>
+            </td>`)
+      row.append(`<td><button class="btn btn-danger">Delete</button></td>`)
+  
+      tableBody.append(row)
+    })
   }
